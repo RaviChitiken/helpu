8750808d3133406a776eced5ba8dcaa95cfc3e51
RaviChitiken <ravichitiken11@outlook.com>
2022-04-17 20:37:31 +0530

    helpu first commit

---
+++ src/App.jsx
@@ -0,0 +1,640 @@
+import "./styles.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="m-3 p-3 parent" draggable="false">
+        <div>
+          <h1 className="helpuLogoCustom">helpu</h1>
+          <p draggable="false" className="slogan">
+            websites that developers may find useful
+          </p>
+        </div>
+
+        <div className="pt-3 child">
+          <p className="">Learn to Code at</p>
+          <a href="https://freecodecamp.org" target="_blank" rel="noreferrer">
+            freeCodeCamp
+          </a>
+          <br />
+          <a
+            href="https://www.theodinproject.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            The Odin Project
+          </a>
+          <br />
+          <a href="https://www.codewars.com/" target="_blank" rel="noreferrer">
+            Codewars
+          </a>
+          <br />
+          <a href="https://javascript30.com" target="_blank" rel="noreferrer">
+            JavaScript30
+          </a>
+          <br />
+          <a href="https://coursera.org" target="_blank" rel="noreferrer">
+            Coursera
+          </a>
+          <br />
+          <a href="https://www.udemy.com/" target="_blank" rel="noreferrer">
+            Udemy
+          </a>
+          <br />
+          <a
+            href="https://www.frontendmentor.io/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          <br />
+          <a href="https://sololearn.com" target="_blank" rel="noreferrer">
+            SoloLearn
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Practice Code at</p>
+          <a href="https://leetcode.com" target="_blank" rel="noreferrer">
+            LeetCode
+          </a>
+          <br />
+          <a href="https://hackerrank.com" target="_blank" rel="noreferrer">
+            HackerRank
+          </a>
+          <br />
+          <a href="https://exercism.io" target="_blank" rel="noreferrer">
+            Exercism
+          </a>
+          <br />
+          <a href="https://codewars.com" target="_blank" rel="noreferrer">
+            Codewars
+          </a>
+          <br />
+          <a href="https://codechef.com" target="_blank" rel="noreferrer">
+            CodeChef
+          </a>
+          <br />
+          <a href="https://codingame.com" target="_blank" rel="noreferrer">
+            CodinGame
+          </a>
+          <br />
+          <a href="https://coderbyte.com" target="_blank" rel="noreferrer">
+            Coderbyte
+          </a>
+          <br />
+          <a href="https://projecteuler.net" target="_blank" rel="noreferrer">
+            Project Euler
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Language Documentation</p>
+          <a
+            href="https://developer.mozilla.org/en-US/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            MDN Web Docs
+          </a>
+          <br />
+          <a href="https://devdocs.io/" target="_blank" rel="noreferrer">
+            DevDocs API Documentation
+          </a>
+          <br />
+          <a href="https://www.w3schools.com/" target="_blank" rel="noreferrer">
+            W3Schools
+          </a>
+          <br />
+          <a href="https://www.w3docs.com/" target="_blank" rel="noreferrer">
+            W3Docs
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Browser Extensions</p>
+          <a
+            href="https://chrome.google.com/webstore/detail/web-developer/bfbameneiokkgbdmiekhjnmfkcnldhhm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Web Developer
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/visbug/cdockenadnadldjbbgcallicgledbeoc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            VisBug
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/pesticide-for-chrome-with/neonnmencpneifkhlmhmfhfiklgjmloi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pesticide
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/css-peeper/mbnbehikldjhnfehhnaidhjhoofhpehk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            CSS Peeper
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/cssviewer/ggfgijbpiheegefliciemofobhmofgce"
+            target="_blank"
+            rel="noreferrer"
+          >
+            CSSViewer
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/svg-export/naeaaedieihlkmdajjefioajbbdbdjgp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            SVG Export
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/json-viewer-pro/eifflpmocdbdmepbjaopkkhbfmdgijcc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            JSON Viewer Pro
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/whatfont/jabopobgcpjmedljpbcaablpmlmfcogm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            WhatFont
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/site-palette/pekhihjiehdafocefoimckjpbkegknoh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Site Palette
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            React Developer Tools
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/wappalyzer/gppongmhjkpfnbhagpmjfkannfbllamg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Wappalyzer
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/colorpick-eyedropper/ohcpnigalekghcmgcdcenkpelffpdolg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ColorPick Eyedropper
+          </a>
+          <br />
+          <a
+            href="https://chrome.google.com/webstore/detail/gofullpage-full-page-scre/fdpohaocaechififmbbbbbknoalclacl"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GoFullPage
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Online Code Editors</p>
+          <a href="https://codesandbox.io/" target="_blank" rel="noreferrer">
+            CodeSandbox
+          </a>
+          <br />
+          <a href="https://jsbin.com/" target="_blank" rel="noreferrer">
+            JS Bin
+          </a>
+          <br />
+          <a href="https://jsfiddle.net/" target="_blank" rel="noreferrer">
+            JSFiddle
+          </a>
+          <br />
+          <a
+            href="https://www.w3schools.com/tryit/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            TryIt Editor
+          </a>
+          <br />
+          <a href="https://codepen.io/" target="_blank" rel="noreferrer">
+            CodePen
+          </a>
+          <br />
+          <a href="https://stackblitz.com/" target="_blank" rel="noreferrer">
+            StackBlitz
+          </a>
+          <br />
+          <a href="https://www.gitpod.io/" target="_blank" rel="noreferrer">
+            Gitpod
+          </a>
+          <br />
+          <a href="https://rextester.com/" target="_blank" rel="noreferrer">
+            Rextester
+          </a>
+          <br />
+          <a href="https://www.onlinegdb.com/" target="_blank" rel="noreferrer">
+            GDB Online Debugger
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Free Hosting Sites</p>
+          <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+            Netlify
+          </a>
+          <br />
+          <a href="https://github.com/" target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+          <br />
+          <a href="https://about.gitlab.com/" target="_blank" rel="noreferrer">
+            GitLab
+          </a>
+          <br />
+          <a href="https://www.heroku.com/" target="_blank" rel="noreferrer">
+            Heroku
+          </a>
+          <br />
+          <a href="https://vercel.com/" target="_blank" rel="noreferrer">
+            Vercel
+          </a>
+          <br />
+          <a
+            href="https://firebase.google.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Firebase
+          </a>
+          <br />
+          <a href="https://aws.amazon.com/" target="_blank" rel="noreferrer">
+            AWS
+          </a>
+          <br />
+          <a href="https://surge.sh/" target="_blank" rel="noreferrer">
+            Surge
+          </a>
+          <br />
+          <a href="https://render.com/" target="_blank" rel="noreferrer">
+            Render
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Images</p>
+          <a href="https://unsplash.com/" target="_blank" rel="noreferrer">
+            Unsplash
+          </a>
+          <br />
+          <a href="https://pixabay.com/" target="_blank" rel="noreferrer">
+            Pixabay
+          </a>
+          <br />
+          <a href="https://www.pexels.com/" target="_blank" rel="noreferrer">
+            Pexels
+          </a>
+          <br />
+          <a
+            href="https://www.vectorstock.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            VectorStock
+          </a>
+          <br />
+          <a
+            href="https://www.freeimages.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            freeImages
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Colors</p>
+          <a href="https://coolors.co/" target="_blank" rel="noreferrer">
+            Coolors
+          </a>
+          <br />
+          <a href="https://colorhunt.co/" target="_blank" rel="noreferrer">
+            Color Hunt
+          </a>
+          <br />
+          <a href="https://picular.co/" target="_blank" rel="noreferrer">
+            Picular
+          </a>
+          <br />
+          <a href="http://colorsafe.co/" target="_blank" rel="noreferrer">
+            Color Safe
+          </a>
+          <br />
+          <a href="https://mycolor.space/" target="_blank" rel="noreferrer">
+            ColorSpace
+          </a>
+          <br />
+          <a
+            href="https://canva.com/colors/color-palette-generator/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Canva
+          </a>
+          <br />
+          <a
+            href="https://color.adobe.com/create/color-wheel"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Adobe Color
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Icons</p>
+          <a href="https://brandeps.com/" target="_blank" rel="noreferrer">
+            BrandEPS
+          </a>
+          <br />
+          <a
+            href="https://www.iconfinder.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Iconfinder
+          </a>
+          <br />
+          <a href="https://icons8.com/" target="_blank" rel="noreferrer">
+            Icons8
+          </a>
+          <br />
+          <a href="https://www.flaticon.com/" target="_blank" rel="noreferrer">
+            Flaticon
+          </a>
+          <br />
+          <a href="https://iconmonstr.com/" target="_blank" rel="noreferrer">
+            iconmonstr
+          </a>
+          <br />
+          <a href="https://freeicons.io/" target="_blank" rel="noreferrer">
+            Freeicons
+          </a>
+          <br />
+          <a href="https://fontawesome.com/" target="_blank" rel="noreferrer">
+            Font Awesome
+          </a>
+          <br />
+          <a href="https://uxwing.com/" target="_blank" rel="noreferrer">
+            UXWing
+          </a>
+          <br />
+          <a
+            href="https://goodstuffnononsense.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Good Stuff No Nonsense
+          </a>
+          <br />
+          <a href="https://dryicons.com/" target="_blank" rel="noreferrer">
+            DryIcons
+          </a>
+          <br />
+          <a href="https://graphicburger.com/" target="_blank" rel="noreferrer">
+            GraphicBurger
+          </a>
+          <br />
+          <a href="https://iconstore.co/" target="_blank" rel="noreferrer">
+            IconStore
+          </a>
+          <br />
+          <a href="https://lordicon.com/" target="_blank" rel="noreferrer">
+            Lordicon
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Illustrations</p>
+          <a href="https://undraw.co/" target="_blank" rel="noreferrer">
+            unDraw
+          </a>
+          <br />
+          <a href="https://www.drawkit.io/" target="_blank" rel="noreferrer">
+            DrawKit
+          </a>
+          <br />
+          <a href="https://storyset.com/" target="_blank" rel="noreferrer">
+            storyset
+          </a>
+          <br />
+          <a href="https://blush.design/" target="_blank" rel="noreferrer">
+            Blush
+          </a>
+          <br />
+          <a
+            href="https://freeillustrations.xyz/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            free illustrations
+          </a>
+          <br />
+          <a href="https://iradesign.io/" target="_blank" rel="noreferrer">
+            IRA Design
+          </a>
+          <br />
+          <a href="https://www.humaaans.com/" target="_blank" rel="noreferrer">
+            humaaans
+          </a>
+          <br />
+          <a
+            href="https://lukaszadam.com/illustrations"
+            target="_blank"
+            rel="noreferrer"
+          >
+            lukaszadam
+          </a>
+          <br />
+          <a href="https://interfacer.xyz/" target="_blank" rel="noreferrer">
+            Interfacer
+          </a>
+          <br />
+          <a href="https://www.manypixels.co/" target="_blank" rel="noreferrer">
+            ManyPixels
+          </a>
+          <br />
+          <a
+            href="https://themeisle.com/illustrations/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Themeisle
+          </a>
+          <br />
+          <a href="https://illlustrations.co/" target="_blank" rel="noreferrer">
+            illustrations
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">CSS Help</p>
+          <a
+            href="https://css3buttongenerator.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            CSS Button Generator
+          </a>
+          <br />
+          <a href="https://css-tricks.com/" target="_blank" rel="noreferrer">
+            CSS-Tricks
+          </a>
+          <br />
+          <a href="http://css3generator.com/" target="_blank" rel="noreferrer">
+            CSS3 Generator
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">JavaScript Help</p>
+          <a href="https://minifier.org/" target="_blank" rel="noreferrer">
+            Minify - JavaScript and CSS minifier
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Design Inspiration</p>
+          <a href="http://10x17.co/" target="_blank" rel="noreferrer">
+            10x17
+          </a>
+          <br />
+          <a href="https://www.behance.net/" target="_blank" rel="noreferrer">
+            Behance
+          </a>
+          <br />
+          <a href="https://dribbble.com/" target="_blank" rel="noreferrer">
+            Dribbble
+          </a>
+          <br />
+          <a href="https://wordpress.com/" target="_blank" rel="noreferrer">
+            Wordpress
+          </a>
+          <br />
+          <a href="https://www.awwwards.com/" target="_blank" rel="noreferrer">
+            Awwwards
+          </a>
+          <br />
+          <a href="https://www.designevo.com/" target="_blank" rel="noreferrer">
+            DesignEvo
+          </a>
+          <br />
+          <a href="https://muz.li/" target="_blank" rel="noreferrer">
+            Muzli
+          </a>
+          <br />
+          <a href="https://gohugo.io/" target="_blank" rel="noreferrer">
+            Hugo
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Logo</p>
+          <a href="https://www.wix.com/" target="_blank" rel="noreferrer">
+            Wix
+          </a>
+          <br />
+          <a href="https://www.canva.com/" target="_blank" rel="noreferrer">
+            Canva
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Mockup</p>
+          <a
+            href="https://uiprint.co/?ref=illlustrations"
+            target="_blank"
+            rel="noreferrer"
+          >
+            uiprint
+          </a>
+          <br />
+          <a
+            href="https://contrauikit.com/?ref=illlustrations"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contra Wireframe Kit
+          </a>
+          <br />
+          <a href="https://www.mockplus.com/" target="_blank" rel="noreferrer">
+            Mockplus
+          </a>
+          <br />
+          <a href="https://www.screely.com/" target="_blank" rel="noreferrer">
+            Screely
+          </a>
+          <br />
+          <a href="https://moqups.com/" target="_blank" rel="noreferrer">
+            Moqups
+          </a>
+          <br />
+          <br />
+        </div>
+        <div className="pt-3 child">
+          <p className="">Online Image Editor</p>
+          <a href="https://pixlr.com/" target="_blank" rel="noreferrer">
+            Pixlr
+          </a>
+          <br />
+          <a href="https://ipiccy.com/" target="_blank" rel="noreferrer">
+            iPiccy
+          </a>
+          <br />
+          <br />
+        </div>
+        <p className="version mt-3">v0.1</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
